8bb86a171da52baede36402e0044c76158ea0043
M Arif Mahthunisnaini <mahthunis@gmail.com>
2021-04-02 23:39:43 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Izn Laravel React
+
+Modern web development boilerplate with laravel, react and inertia
+
+## Technology
+1. Laravel (http://laravel.com)
+2. React (http://react.com)
+3. Inertia (http://inertiajs.com)
+
+## Instalation
+1. Clone project  
+```sh
+git clone project_url project_folder
+```
+2. Install Laravel
+```sh
+composer install
+cp .env.example .env
+php artisan key:generate
+```
+3. Prepare the database
+3.1. Create new database
+3.2. Change database settings in .env
+3.3. Importing database with run:
+```sh
+php artisan migrate
+php artisan db:seed
+```
+4. Install frontend
+```sh
+npm install
+```
+
+## Developing
+```sh
+npm run dev
+```
+
+With watch files
+```sh
+npm run watch
+```
+
+## Production
+```sh
+npm run production
+```
---
+++ webpack.mix.js
@@ -0,0 +1,36 @@
+const mix = require('laravel-mix');
+const path = require('path');
+const cssImport = require('postcss-import');
+const cssNesting = require('postcss-nesting');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .react()
+    .sass('resources/sass/app.scss', 'public/css', {}, [
+        // prettier-ignore
+        cssImport(),
+        cssNesting(),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+   .webpackConfig({
+        output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
+        resolve: {
+            alias: {
+                '@': path.resolve('resources/js'),
+                '@pages': path.resolve('resources/js/Pages'),
+            },
+        },
+    })
+    .version()
+    .sourceMaps();
